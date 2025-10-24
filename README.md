4e93e61d2525778b496687086cc4fa8aba5ee993
John Roa <john.roa@uip.edu.pa>
2025-10-24 16:59:28 -0400

    Actualización del README por John

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # proyecto_colaborativo_python
 Proyecto colaborativo en Python con tres módulos y participación grupal.
+### Cambios realizados por John Roa
+- Actualización del README para prueba de Pull Request.
---
+++ cuento.py
@@ -0,0 +1,29 @@
+def imprimir_cuento():
+    cuento = """
+    EL PUENTE DE LOS SUEÑOS
+
+    En un pequeño pueblo a orillas del río Chagres vivía una joven llamada Maritza,
+    quien todos los días cruzaba un viejo puente de madera para ir a la escuela.
+    Soñaba con ser ingeniera para construir un puente nuevo, fuerte y seguro,
+    que resistiera las lluvias y el paso del tiempo.
+
+    Los vecinos se reían y decían:
+    “¡Eso es cosa de hombres, muchacha!”,
+    pero ella nunca se rindió. 
+    Cada tarde, después de hacer sus tareas, ayudaba a su papá a reparar tablas rotas
+    y a entender cómo funcionaban los cimientos.
+
+    Pasaron los años. Maritza estudió en la Universidad Tecnológica de Panamá
+    y, con mucho esfuerzo, se graduó con honores.
+    Su primer proyecto fue justo en su pueblo natal:
+    diseñó un nuevo puente sobre el río Chagres, moderno y resistente.
+
+    El día de la inauguración, los mismos vecinos que antes dudaban
+    aplaudieron mientras ella cortaba la cinta tricolor.
+    Con lágrimas en los ojos, Maritza dijo:
+    “Este puente no solo une orillas… une sueños.”
+
+    Y desde entonces, aquel puente se conoce como “El Puente de los Sueños”,
+    símbolo del esfuerzo, la fe y la determinación del pueblo panameño.
+    """
+    print(cuento)
